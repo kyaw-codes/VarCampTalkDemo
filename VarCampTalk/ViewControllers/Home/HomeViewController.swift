2cfe4a88fc27e2470e43d6907c336f33ea5bd948
kyaw-codes <kyaw.codes@gmail.com>
2022-05-07 18:08:41 +0630

    chore: remove logging

--- VarCampTalk/ViewControllers/Home/HomeViewController.swift
+++ VarCampTalk/ViewControllers/Home/HomeViewController.swift
@@ -14,8 +14,6 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print("Hello, welcome from VarCamp Talk!")
     }
 
 }
